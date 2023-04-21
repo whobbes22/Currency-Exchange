2617f8f1ef3a1107b8150713e6fdcf0e2407a65f
Joe W <whobbes22@gmail.com>
2023-04-21 11:59:20 -0700

    updated error handeling

--- src/index.js
+++ src/index.js
@@ -10,28 +10,58 @@ async function getExchange(amount,countryCode1,countryCode2){
   if(response.result === "success"){
     printExchangeElements(amount,response,countryCode2);
   } else {
-    printError(response);
+    printErrorAPI(response);
   }
 }
 
-function printExchangeElements(apiResponse){
-  document.querySelector("#results").innerText = `conversion to: ${apiResponse.conversion_rates.AUD}`;
-}
 
-function printError(apiResponse){
-  document.querySelector("#results").innerText = `Error... ${apiResponse}`;
-}
+function checkCode(code){
+  const acceptedCodes = ["USD","AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BRL","BSD","BTN","BWP","BYN","BZD","CAD","CDF","CHF","CLP","CNY","COP","CRC","CUP","CVE","CZK","DJF","DKK","DOP","DZD","EGP","ERN","ETB","EUR","FJD","FKP","FOK","GBP","GEL","GGP","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR", "ILS","IMP","INR","IQD","IRR", "ISK","JEP","JMD","JOD","JPY","KES","KGS","KHR","KID","KMF","KRW","KWD","KYD","KZT","LAK", "LBP", "LKR","LRD","LSL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRU","MUR","MVR","MWK","MXN","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SLE","SLL", "SOS","SRD","SSP","STN","SYP","SZL","THB","TJS","TMT","TND","TOP","TRY","TTD","TVD","TWD","TZS","UAH","UGX","UYU","UZS", "VES","VND", "VUV","WST","XAF","XCD","XDR","XOF","XPF","YER","ZAR","ZMW","ZWL"];
 
-function handleFormSubmission(event){
-  event.preventDefault();
-  const amount = document.querySelector("amount").value
-  const countCode1 = document.querySelector("#code1").value;
-  const countCode2 = document.querySelector("#code2").value;
-  getExchange(amount,countCode1,countCode2);
+  return acceptedCodes.includes(code.toUpperCase());
 }
 
 // ui logic
 
+
+function printExchangeElements(apiResponse){
+  document.querySelector("#results").innerText = `conversion to: ${apiResponse["conversion_rates"]}`;
+  console.log(apiResponse["conversion_rates"]);
+  console.log(apiResponse["conversion_rates"].USD);
+}
+
+function printErrorAPI(apiResponse){
+  document.querySelector("#results").innerText = `Error... ${apiResponse["error-type"]}`;
+}
+
+function printErrorInvalidCode(countCode1,check1,countCode2,check2){
+  let valid1 = "";
+  let valid2 = "";
+  if(!check1){
+    valid1 = "NOT ";
+  }
+  if(!check2){
+    valid2 = "NOT ";
+  }
+  document.querySelector("#results").innerText =`Your first input "${countCode1}" is ${valid1}a valid response. Your second input "${countCode2}" is ${valid2}a valid response. Please refer to the Wikipedia page and give the correct 3 character country code. `;
+}
+
+function handleFormSubmission(event){
+  event.preventDefault();
+  const amount = document.querySelector("#amount").value;
+  const countCode1 = document.querySelector("#code1").value;
+  const countCode2 = document.querySelector("#code2").value;
+  const check1 = checkCode(countCode1);
+  const check2 = checkCode(countCode2);
+  if(check1 && check2){
+    getExchange(amount,countCode1,countCode2);
+  } else {
+    printErrorInvalidCode(countCode1,check1,countCode2,check2);
+  }
+}
+
+
 window.addEventListener("load", function(){
   document.querySelector("form").addEventListener("submit",handleFormSubmission);
 });
+
