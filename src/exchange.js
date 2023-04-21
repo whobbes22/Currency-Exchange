a9b9ca01501ce40fa86124398ea2f837bafae80c
Joe W <whobbes22@gmail.com>
2023-04-21 09:32:16 -0700

    add exchange api

---
+++ src/exchange.js
@@ -0,0 +1,16 @@
+export default class Exchange {
+  static async getExchange(countryCode){
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${countryCode}`);
+      const jsoinifiedResponse = await response.json();
+      if(!response.ok){
+        //error message
+        throw new Error(`${response.status}`);
+      }
+      return jsoinifiedResponse;
+      } catch(error) {
+        return error;
+      }
+  }
+
+}
